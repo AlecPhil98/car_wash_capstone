a37f2c1e6d725b74187bc478fe113e19163b50df
Alec Philavong <alecphil@live.com>
2024-07-15 10:51:34 -0500

    add services data in js

---
+++ pages/js/cart.js
@@ -0,0 +1,46 @@
+"use strict"
+
+const servicePackages = [
+    {
+        name: "Classic Wash",
+        price: 25,
+        services: 3,
+        desc: "Gentle hand wash using premium automotive shampoos to lift dirt and contaminants without scratching the paint. Careful attention is given to intricate detailsand hard-to-reach areas."
+    },
+    {
+        name: "Custom Car Wash",
+        price: 50,
+        services: 6,
+        desc: "Deep cleaning and shining of wheels and tires to remove brake dust, road tar, and other contaminants, enhancing the overall aesthetic appeal of your classic car"
+    },
+    {
+        name: "Ultimate Car Wash",
+        price: 75,
+        services: 12,
+        desc:"Protective Coatings: Application of high-quality sealants and waxes toprovide long-lasting protection against UV rays, pollutants, and environmental hazards,preserving the beauty and value of your classic vehicle for years to come."
+    },
+    {
+        name: "Paint Correction",
+        price: 550,
+        services: 6,
+        desc:"Paint Correction: Professional polishing and buffing to restore the luster and shine of your classic car's paintwork, removing swirl marks, scratches, and oxidation for a flawless finish"
+    },
+    {
+        name: "Interior Clean",
+        price: 220,
+        services: 7,
+        desc:"Interior Detailing: Thorough cleaning and conditioning of the interior surfaces, including upholstery, carpets, dashboard, and trim, to rejuvenate and protect against wear and tear"
+    },
+    {
+        name: "Mega Car Wash",
+        price: 845,
+        services: 25,
+        desc:"Includes all the services listed beforehand"
+    }
+]
+
+
+window.onload = () => {
+
+
+}
