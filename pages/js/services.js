18aa4f2568d6d8a7c14c8262900f23947c09b634
Alec Philavong <alecphil@live.com>
2024-07-15 21:14:15 -0500

    add basic stucture

---
+++ pages/js/services.js
@@ -0,0 +1,16 @@
+"use strict"
+
+
+
+winddow.onload=()=>{
+
+    const addToCart = document.querySelector("#add_to_cart")
+    addToCart.addEventListener("#submit",addToCartForm)
+}
+
+
+function addToCartForm(event){
+
+    event.preventDefault();
+
+}
